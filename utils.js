150d4cc5ff8bd3fff59d9baa8ab199f255c114ba
ChenDeli <419287484@qq.com>
2020-04-01 14:44:09 +0800

    更新工具类

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import XLSX from "XLSX";
 
 // 自动添加数字单位 传入num数字 unit个 或者其他单位
 const autoGetUnit = (num = 0, unit = '', tofixed = 1) => {
@@ -110,6 +111,100 @@ const JsonParse = string => {
   return value;
 };
 
+//读取Xlsx静态表格文件
+
+const readXlsx = (fileBase64) => {
+  // 将base64转换为blob
+  const dataURLtoBlob = (dataurl) => {
+    const arr = dataurl.split(',')
+    const mime = arr[0].match(/:(.*?);/)[1]
+    const bstr = atob(arr[1])
+    let n = bstr.length
+    const u8arr = new Uint8Array(n);
+    // eslint-disable-next-line no-plusplus
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+  }
+  const blob = dataURLtoBlob(fileBase64);
+  const fileReader = new FileReader();
+  fileReader.readAsBinaryString(blob)
+
+  return fileReader
+}
+
+const dealData = (fileBase64, getdata, type) => {
+  if (!fileBase64 || !getdata || !type) {
+    return
+  }
+  // type为传入参数
+  // getdata为取值用的函数
+  const fileReader = readXlsx(fileBase64)
+  fileReader.onload = (ev) => {
+    let workbook = null
+    let datas = [];
+    try {
+      const data = ev.target.result
+      workbook = XLSX.read(data, {
+        type: 'binary',
+      })// 以二进制流方式读取得到整份excel表格对象
+      //  // 存储获取到的数据
+    } catch (e) {
+      // console.log('文件类型不正确');
+    }
+    // 表格的表格范围，可用于判断表头是否数量是否正确
+    // let fromTo = '';
+    // 遍历每张表读取
+    for (const sheet in workbook.Sheets) {
+      // if (workbook.Sheets.hasOwnProperty(sheet)) {
+      if (Object.prototype.hasOwnProperty.call(workbook.Sheets, sheet)) {
+        // fromTo = workbook.Sheets[sheet]['!ref'];
+        // console.log(fromTo);
+        datas = datas.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet]));
+        // break; // 如果只取第一张表，就取消注释这行
+      }
+    }
+    getdata(datas, type)
+  }
+}
+
+// 处理数据
+// componentDidMount(){
+
+//   dealData(province, this.getdata, "省份区域")
+//   // dealData(thisTrades, this.getdata, "所属行业")
+// }
+
+// getdata = (arr, type) => {
+//   const { newMenuData } = this.state
+//   this.setState({
+//     newMenuData: newMenuData.map(ele => {
+//       const { key } = ele
+//       if (key === type) {
+//         ele.children = arr.map(item => {
+//           const { code, name } = item
+//           return ({
+//             ...item,
+//             key: code,
+//             title: name,
+//             search: code,
+//             icon: null,
+//           })
+//         })
+//       }
+//       return ele
+//     }),
+//   })
+// }
+
+// 千分位数字
+const formatNum=(num)=> {
+  const reg = /\d{1,3}(?=(\d{3})+$)/g;
+  return (`${num}`).replace(reg, '$&,');
+}
+
+
 export default {
   numberToFixed,
   isDecimal,
@@ -121,4 +216,15 @@ export default {
   beMathRound,
   setPercentFloat,
   JsonParse,
+  dealData,
+  formatNum,
 };
+
+// 简单的number 字符格式化输出
+export const formatNumberToString=(num, degree = 2)=> {
+	let value = Number(num);
+	value = isNaN(value) ? 0 : value;
+	if (degree === 0) return Math.round(value);
+	const power = Math.pow(10, degree);
+	return Math.round(value * power) / power;
+}
